6990b9c094db7b16a6ab2d1e792df50a1dc4825b
Chetan Jain <chetanplrch@gmail.com>
2023-04-13 22:20:02 -0400

    update the files

--- test.py
+++ test.py
@@ -3,14 +3,12 @@ import Construct_AST
 import Evaluation
 
 t = Tokenizer.Tokenizer(open('expression.txt').read())
-# print(t.tokenize())
-tokens = t.tokenized_lines()
-for l in tokens:
-    print(l)
+token_lines = t.char_without_type_tokenized_lines()
+for tokens in token_lines:
+    c_a = Construct_AST.Construct_AST()
+    ast = c_a.construct(tokens)
+    c_a.print_ast(ast)
+    e = Evaluation.Evaluate()
+    v = e.evaluate(ast)
+    print(v)
 
-# c_a = Construct_AST.Construct_AST()
-# ast = c_a.construct(tokens)
-
-# e = Evaluation.Evaluate()
-# v = e.evaluate(ast)
-# print(v)

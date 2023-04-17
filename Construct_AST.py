7226ef10eb24e4cb4da1b4edb6a02efc068c03e3
Chetan Jain <chetanplrch@gmail.com>
2023-04-17 12:16:43 -0400

    raise parse error

--- Construct_AST.py
+++ Construct_AST.py
@@ -2,79 +2,76 @@ import Errors
 
 class Construct_AST:
     def construct(self, tokens):
-        precedence = {
-            '<': (1, 'left'),
-            '>': (1, 'left'),
-            '==': (1, 'left'),
-            '!=': (1, 'left'),
-            '<=': (1, 'left'),
-            '>=': (1, 'left'),
-            '&&': (1, 'left'),
-            '||': (1, 'left'),
-            '+': (2, 'left'),
-            '-': (2, 'left'),
-            '*': (3, 'left'),
-            '/': (3, 'left'),
-            '%': (3, 'left'),
-            '^': (4, 'right')
-        }
+        try:
+            precedence = {
+                '<': (1, 'left'),
+                '>': (1, 'left'),
+                '==': (1, 'left'),
+                '!=': (1, 'left'),
+                '<=': (1, 'left'),
+                '>=': (1, 'left'),
+                '&&': (1, 'left'),
+                '||': (1, 'left'),
+                '+': (2, 'left'),
+                '-': (2, 'left'),
+                '*': (3, 'left'),
+                '/': (3, 'left'),
+                '%': (3, 'left'),
+                '^': (4, 'right')
+            }
 
-        ast_stack = []
-        op_stack = []
+            ast_stack = []
+            op_stack = []
 
-        for token in tokens:
-            if token in precedence:
-                curr_precedence, curr_associativity = precedence[token]
-                while op_stack and op_stack[-1] != '(' and (precedence[op_stack[-1]][0] > curr_precedence or
-                                                            (precedence[op_stack[-1]][0] == curr_precedence and curr_associativity == 'left')):
-                    operator = op_stack.pop()
-                    right = ast_stack.pop()
-                    left = ast_stack.pop()
+            for token in tokens:
+                if token in precedence:
+                    curr_precedence, curr_associativity = precedence[token]
+                    while op_stack and op_stack[-1] != '(' and (precedence[op_stack[-1]][0] > curr_precedence or
+                                                                (precedence[op_stack[-1]][0] == curr_precedence and curr_associativity == 'left')):
+                        operator = op_stack.pop()
+                        right = ast_stack.pop()
+                        left = ast_stack.pop()
+                        ast_stack.append({
+                            'type': 'Binary',
+                            'operator': operator,
+                            'left': left,
+                            'right': right
+                        })
+                    op_stack.append(token)
+                elif token == '(':
+                    op_stack.append(token)
+                elif token == ')':
+                    while op_stack and op_stack[-1] != '(':
+                        operator = op_stack.pop()
+                        right = ast_stack.pop()
+                        left = ast_stack.pop()
+                        ast_stack.append({
+                            'type': 'Binary',
+                            'operator': operator,
+                            'left': left,
+                            'right': right
+                        })
+                    op_stack.pop()
+                else:
                     ast_stack.append({
-                        'type': 'Binary',
-                        'operator': operator,
-                        'left': left,
-                        'right': right
+                        'type': 'Literal' if self.is_number(token) else 'Identifier',
+                        'value': token
                     })
-                op_stack.append(token)
-            elif token == '(':
-                op_stack.append(token)
-            elif token == ')':
-                while op_stack and op_stack[-1] != '(':
-                    operator = op_stack.pop()
-                    right = ast_stack.pop()
-                    left = ast_stack.pop()
-                    ast_stack.append({
-                        'type': 'Binary',
-                        'operator': operator,
-                        'left': left,
-                        'right': right
-                    })
-                op_stack.pop()
-            else:
+
+            while op_stack:
+                operator = op_stack.pop()
+                right = ast_stack.pop()
+                left = ast_stack.pop()
                 ast_stack.append({
-                    'type': 'Literal' if self.is_number(token) else 'Identifier',
-                    'value': token
+                    'type': 'Binary',
+                    'operator': operator,
+                    'left': left,
+                    'right': right
                 })
-
-        while op_stack:
-            # if op_stack[-1] == '(':
-            #     raise SyntaxError('Mismatched parentheses')
-            operator = op_stack.pop()
-            right = ast_stack.pop()
-            left = ast_stack.pop()
-            # if operator == '/' and right['value'] == '0':
-            #     raise ZeroDivisionError('Division by zero')
-            ast_stack.append({
-                'type': 'Binary',
-                'operator': operator,
-                'left': left,
-                'right': right
-            })
-
-        # if len(ast_stack) != 1:
-        #     raise Errors.ParseError('Invalid expression')
-
+            # if len(ast_stack) != 1:
+            #     raise Errors.ParseError('Invalid expression')
+        except:
+            raise Errors.ParseError('parse error')
         return ast_stack[0]
 
     def print_ast(self, node, prefix='', is_left=True):

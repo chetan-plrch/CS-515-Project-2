a78afabbd2c3fc567b9b614db7c0047b3ef5ac38
Chetan Jain <chetanplrch@gmail.com>
2023-04-16 23:04:16 -0400

    remove un-necessary files

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-import os
-import signal
-import subprocess
 import Tokenizer
-import Construct_AST
-import Evaluation
 import Printer
 
 
-file1 = open('expression.txt', 'r')
-Lines = file1.read()
+while True:
+    line = input()
+    t = Tokenizer.Tokenizer(line)
+    Lines = t.text
 
-t = Tokenizer.Tokenizer(Lines)
-Lines=t.text
-
-
-x=Printer.Printer()
-for i in Lines.split("\n"):
-    x.assigner(i)
-
-
-# t = Tokenizer.Tokenizer(open('expression.txt').read())
-# tokens = t.clean_tokenize()
-# c_a = Construct_AST.Construct_AST()
-# ast = c_a.construct(tokens)
-
-# e = Evaluation.Evaluate()
-# v = e.evaluate(ast)
-# print(v)
-
-
-# print('s')
-# start a subprocess
-# p = subprocess.Popen(["bc", "./expression.txt"], stdout=subprocess.PIPE)
-# out, err = p.communicate()
-# print('out:', out, err)
+    x = Printer.Printer()
+    x.assigner(Lines)
 
